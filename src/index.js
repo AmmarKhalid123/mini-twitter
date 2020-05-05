1c6c74e23058f0aa6e5995790a5787290dabdb66
AmmarKhalid123 <ammarkhalid237@gmail.com>
2020-05-05 11:37:24 +0500

    Dependencies installed

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import reducer from './reducers'
+
+const store = createStore(reducer)
 
 ReactDOM.render(<App />, document.getElementById('root'))
