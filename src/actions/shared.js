5aabcb77df5c02612713aa577dab8fd8e7acf50f
AmmarKhalid123 <ammarkhalid237@gmail.com>
2020-05-05 11:11:02 +0500

    Initialized

---
+++ src/actions/shared.js
@@ -0,0 +1,17 @@
+import { getInitialData } from '../utils/api';
+import { recieveUsers} from './users';
+import { recieveTweets} from './tweets';
+import {setAuthedUser} from './authedUser';
+
+const AUTHED_ID = 'tylermcginnis';
+
+export function handleInitialData () {
+    return (dispatch) => {
+        return getInitialData()
+                .then(({users, tweets}) => {
+                    dispatch(recieveUsers(users))
+                    dispatch(recieveTweets(tweets))
+                    dispatch(setAuthedUser(AUTHED_ID))
+                })
+    }
+}
